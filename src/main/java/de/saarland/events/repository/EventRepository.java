3e346b2373759099f07944fa5c69706c7d23b3c8
Anton Marenholts <ahtoh.ukr@gmail.com>
2025-09-22 10:49:47 +0200

    working

--- src/main/java/de/saarland/events/repository/EventRepository.java
+++ src/main/java/de/saarland/events/repository/EventRepository.java
@@ -13,6 +13,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
+
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.util.List;
